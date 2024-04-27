bb3e34c08b82f0f31db8206fbb6247c7538ab7c5
shree <sc0844985@gmail.com>
2024-04-27 10:09:52 +0530

    week 1 complete of the course

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get ,Param,Post, Query, Req, Res  } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AnswerDto } from './dto/app.dto';
+import { stat } from 'fs';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Req() req, @Res() res) {
+    console.log(req.headers)
+    res.status(200).json({
+      res: this.appService.getHello()
+    })
   }
+
+  @Get('/askquestion')
+  askQuestion():string{
+    return this.appService.askQuestion();
+  }
+
+  @Post('/answer')
+  getAnswer(@Body() answer:AnswerDto,@Req() req,   @Res() res){
+    let status,response;
+    if(req.body.answer === 'yes'){
+      response = "It is yes";
+      status = 200
+    }else{
+      response = "It is no"
+      status = 400
+    }
+    return res.status(status).json({res : response});
+  }
+
+  // @Get('/page')
+  // getQueryParams(@Query('name') name , @Query('age') age):string{
+  //   return `${name} , ${age}`;
+  // }
+
+  // @Get(':id')
+  // getId(@Param('id') id):string{
+  //   return `${id}`;
+  // }
+
+ 
+
+ 
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,7 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  askQuestion():string{
+    return '<h2>How are you ? </h2>';
+  }
 }
